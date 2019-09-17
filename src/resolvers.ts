83cfdc1e6172b3565927f575b7fcb615b74db328
Ted Kim <yenafirst91@gmail.com>
2019-09-16 20:02:41 -0400

    Add user login

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -16,6 +16,19 @@ export const resolvers = {
       }).save();
 
       return true;
+    },
+    login: async (_: any, { email, password }) => {
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return null;
+      }
+
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) {
+        return null;
+      }
+
+      return user;
     }
   }
 }

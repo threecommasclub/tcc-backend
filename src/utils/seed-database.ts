dda2c57916e377f1e7bad8d0d27b0859a8586268
Ted Kim <dev.ted.kim@gmail.com>
2019-09-23 01:11:16 -0400

    Drop schema when seeding data

--- src/utils/seed-database.ts
+++ src/utils/seed-database.ts
@@ -8,7 +8,25 @@ import { seedData } from '../constants/seed-data';
 (async () => {
   console.log('Beginning DB seed task.');
 
-  const conn = await createConnection();
+  const conn = await createConnection({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'tcc',
+    password: 'tcc_password',
+    database: 'tcc',
+    synchronize: true,
+    dropSchema: true,
+    logging: false,
+    entities: ['src/entities/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber',
+    },
+  });
   console.log('DB connected.');
 
   const companies = await Company.create(seedData);

84c812a8369192d365fbf55d707432d72930dc0a
Jay Park <jay.jongcheol.park@gmail.com>
2019-09-30 23:50:29 -0400

    Fix eslint errors

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -4,9 +4,6 @@ import jwt from 'jsonwebtoken';
 import { createAccessToken, createRefreshToken } from '../utils/auth/jwt';
 import { User } from '../entities/user';
 
-// TODO: lint
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie('jid', token, {
     httpOnly: true,
@@ -14,26 +11,35 @@ export const sendRefreshToken = (res: Response, token: string) => {
   });
 };
 
+interface Payload {
+  userId?: string;
+  accessToken?: string;
+  tokenVersion?: number;
+}
+
 export const refreshAccessToken = async (req: Request, res: Response) => {
   const token = req.cookies.jid;
-  if (!token) {
+  if (!token || !process.env.REFRESH_TOKEN_SECRET) {
     return res.send({ ok: false, accessToken: '' });
   }
-  let payload: any = null;
+
   try {
-    payload = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    const payload: Payload | string = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+    if (typeof payload === 'string') {
+      return res.send({ ok: false, accessToken: '' });
+    }
+    // token is valid and send back an access token
+    const user = await User.findOne({ id: payload.userId });
+    if (!user) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+    if (user.tokenVersion !== payload.tokenVersion) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+    sendRefreshToken(res, createRefreshToken(user));
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
   } catch (err) {
     console.log(err);
     return res.send({ ok: false, accessToken: '' });
   }
-  // token is valid and send back an access token
-  const user = await User.findOne({ id: payload.userId });
-  if (!user) {
-    return res.send({ ok: false, accessToken: '' });
-  }
-  if (user.tokenVersion !== payload.tokenVersion) {
-    return res.send({ ok: false, accessToken: '' });
-  }
-  sendRefreshToken(res, createRefreshToken(user));
-  return res.send({ ok: true, accessToken: createAccessToken(user) });
 };

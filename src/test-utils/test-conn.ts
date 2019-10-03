d042d9722e0a5cb08109f4fcbcfafc0cd8169b92
Jay Park <jay.jongcheol.park@gmail.com>
2019-10-02 22:56:56 -0400

    Fix no-inferrable-types

--- src/test-utils/test-conn.ts
+++ src/test-utils/test-conn.ts
@@ -1,6 +1,6 @@
 import { createConnection } from 'typeorm';
 
-export const testConn = (drop: boolean = false) => {
+export const testConn = (drop = false) => {
   return createConnection({
     type: 'mysql',
     host: 'localhost',
